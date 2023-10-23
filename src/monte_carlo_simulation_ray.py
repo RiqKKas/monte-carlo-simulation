e055fbc6981f78def2b93c20b0e869be057f32f3
the-riquelme <riquelmedamiaosilva@gmail.com>
2023-10-23 13:43:04 -0300

    :recycle: refactor: refactor unit square calculation function

--- src/monte_carlo_simulation_ray.py
+++ src/monte_carlo_simulation_ray.py
@@ -8,7 +8,7 @@ def monte_carlo_simulation(num_samples):
     inside_circle = 0
     for _ in range(num_samples):
         x, y = random.random(), random.random()
-        if x*2 + y*2 <= 1:
+        if x**2 + y**2 <= 1:
             inside_circle += 1
             
     return inside_circle
@@ -22,26 +22,26 @@ def estimate_pi(num_samples, num_clusters):
     
     return pi_estimate
 
+if __name__ == "__main__":
+  num_samples = 10000000
 
-num_samples = 100000000
+  # Inicializa o ambiente Ray
+  ray.init()
 
-# Inicializa o ambiente Ray
-ray.init()
+  # Obtem informações sobre os recursos disponiveis
+  cluster_resources = ray.cluster_resources()
 
-# Obtem informações sobre os recursos disponiveis
-cluster_resources = ray.cluster_resources()
+  # Obtem o numero de CPUs disponiveis
+  num_clusters = int(cluster_resources.get("CPU", 0))
 
-# Obtem o numero de CPUs disponiveis
-num_clusters = int(cluster_resources.get("CPU", 0))
+  start_time = time.time()
+  pi_estimate = estimate_pi(num_samples, num_clusters)
+  end_time = time.time()
 
-start_time = time.time()
-pi_estimate = estimate_pi(num_samples, num_clusters)
-end_time = time.time()
+  print("=> Execução Distribuida")
+  print(f"π Estimado: {pi_estimate}")
+  print(f"π Real: {3.14159265358979323846}")
+  print(f"Tempo Gasto: {end_time - start_time} segundos") 
+  print(f"Total de CPUs: {num_clusters}")
 
-print("=> Execução Distribuida")
-print(f"π Estimado: {pi_estimate}")
-print(f"π Real: {3.14159265358979323846}")
-print(f"Tempo Gasto: {end_time - start_time} segundos") 
-print(f"Total de CPUs: {num_clusters}")
-
-ray.shutdown()
+  ray.shutdown()
--- src/monte_carlo_simulation_without_ray.py
+++ src/monte_carlo_simulation_without_ray.py
@@ -6,7 +6,7 @@ def monte_carlo_simulation(num_samples):
     inside_circle = 0
     for _ in range(num_samples):
         x, y = random.random(), random.random()
-        if x*2 + y*2 <= 1:
+        if x**2 + y**2 <= 1:  # Correção: x^2 + y^2 em vez de x*2 + y*2
             inside_circle += 1
             
     return inside_circle
@@ -18,18 +18,18 @@ def estimate_pi(num_samples, num_clusters):
     
     return pi_estimate
 
+if __name__ == "__main__":
+    num_samples = 10000000
 
-num_samples = 100000000
+    # Obtem o numero de CPUs disponiveis na maquina
+    num_clusters = os.cpu_count()
 
-# Obtem o numero de CPUs disponiveis na maquina
-num_clusters = os.cpu_count()
+    start_time = time.time()
+    pi_estimate = estimate_pi(num_samples, num_clusters)
+    end_time = time.time()
 
-start_time = time.time()
-pi_estimate = estimate_pi(num_samples, num_clusters)
-end_time = time.time()
-
-print("=> Execução Centralizada")
-print(f"π Estimado: {pi_estimate}")
-print(f"π Real: {3.14159265358979323846}")
-print(f"Tempo Gasto: {end_time - start_time} segundos") 
-print(f"Total de CPUs: {num_clusters}")
+    print("=> Execução Centralizada")
+    print(f"π Estimado: {pi_estimate}")
+    print(f"π Real: {3.14159265358979323846}")
+    print(f"Tempo Gasto: {end_time - start_time} segundos") 
+    print(f"Total de CPUs: {num_clusters}")
