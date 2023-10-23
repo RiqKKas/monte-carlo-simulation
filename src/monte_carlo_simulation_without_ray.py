dbf2bb707faebae9c1ae20e8a24def5cc1974018
the-riquelme <riquelmedamiaosilva@gmail.com>
2023-10-23 13:47:29 -0300

    :recycle: refactor: refactor unit square calculation function

--- src/monte_carlo_simulation_without_ray.py
+++ src/monte_carlo_simulation_without_ray.py
@@ -6,7 +6,7 @@ def monte_carlo_simulation(num_samples):
     inside_circle = 0
     for _ in range(num_samples):
         x, y = random.random(), random.random()
-        if x**2 + y**2 <= 1:  # Correção: x^2 + y^2 em vez de x*2 + y*2
+        if x**2 + y**2 <= 1:
             inside_circle += 1
             
     return inside_circle
